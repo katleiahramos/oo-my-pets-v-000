564a72d72c73a36ec782334b5064f5fb417333c9
Katleiah Ramos <katleiahramos@gmail.com>
2018-04-04 19:12:33 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -70,7 +70,7 @@ class Owner
 
   def sell_pets
     #binding.pry
-    self.pets.each do |species,pets|
+    pets.each do |species,pets|
       pets.each do |pet|
         pet.mood = "nervous"
         pets.clear
