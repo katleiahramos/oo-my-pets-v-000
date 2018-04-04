fe895b458cc2e7215fad848d45f7978151ad298b
Katleiah Ramos <katleiahramos@gmail.com>
2018-04-04 17:13:36 +0000

    automatically backed up by learn

--- spec/owner_spec.rb
+++ spec/owner_spec.rb
@@ -16,7 +16,7 @@ describe Owner do
     it "can count how many owners have been created" do
       Owner.reset_all
       Owner.new("human")
-      binding.pry
+      
       expect(Owner.count).to eq(1)
     end
 
